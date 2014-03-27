46a25f4b28640a5460e560ed5987e871ec1e77b6
Todd Schneider <todd.w.schneider@gmail.com>
2014-03-27 17:13:45 -0400

    fetch_and_parse

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -22,9 +22,12 @@ class Talk < ActiveRecord::Base
       end
     end
     
+    def fetch_and_parse(url)
+      Nokogiri::HTML(RestClient.get(url))
+    end
+    
     def create_railsconf_2014_talks
-      html = RestClient.get("http://railsconf.com/program")
-      doc = Nokogiri::HTML(html)
+      doc = fetch_and_parse("http://railsconf.com/program")
 
       doc.css(".presentation .session").each do |elm|
         title = elm.css("header h1").inner_text
@@ -37,11 +40,8 @@ class Talk < ActiveRecord::Base
     end
     
     def create_railsconf_2013_talks
-      talks_html = RestClient.get("http://railsconf.com/2013/talks.html")
-      bios_html = RestClient.get("http://railsconf.com/2013/speakers.html")
-
-      talks_doc = Nokogiri::HTML(talks_html)
-      bios_doc = Nokogiri::HTML(bios_html)
+      talks_doc = fetch_and_parse("http://railsconf.com/2013/talks.html")
+      bios_doc = fetch_and_parse("http://railsconf.com/2013/speakers.html")
 
       talks_doc.css(".talk").each do |elm|
         title = elm.css("h4").inner_text
@@ -56,8 +56,7 @@ class Talk < ActiveRecord::Base
     end
     
     def create_railsconf_2012_talks
-      html = RestClient.get("http://lanyrd.com/2012/railsconf/")
-      doc = Nokogiri::HTML(html)
+      doc = fetch_and_parse("http://lanyrd.com/2012/railsconf/")
       
       doc.css(".session-detail").each do |elm|
         title = elm.css("h3 a").inner_text
@@ -65,9 +64,7 @@ class Talk < ActiveRecord::Base
         
         next if title.blank? || speaker.blank? || title.downcase.squish.starts_with?("keynote:")
         
-        talk_url = "http://lanyrd.com#{elm.css("h3 a").first["href"]}"
-        talk_html = RestClient.get(talk_url)
-        talk_doc = Nokogiri::HTML(talk_html)
+        talk_doc = fetch_and_parse("http://lanyrd.com#{elm.css("h3 a").first["href"]}")
         
         abstract = talk_doc.css(".abstract").inner_text
         bio = talk_doc.css(".profile-longdesc").inner_text.presence || 
@@ -84,14 +81,12 @@ class Talk < ActiveRecord::Base
     end
     
     def create_railsconf_oreilly_talks_by_year(year)
-      sessions_html = RestClient.get("http://en.oreilly.com/rails#{year}/public/schedule/topic/General")
-      sessions_doc = Nokogiri::HTML(sessions_html)
+      sessions_doc = fetch_and_parse("http://en.oreilly.com/rails#{year}/public/schedule/topic/General")
     
       sessions_doc.css(".en_session_title .url").each do |a|
         next if a.inner_text.to_s.downcase.squish == "lightning talks"
       
-        html = RestClient.get("http://en.oreilly.com#{a["href"]}")
-        doc = Nokogiri::HTML(html)
+        doc = fetch_and_parse("http://en.oreilly.com#{a["href"]}")
       
         title = doc.css("h1.summary").inner_text
         speaker = doc.css(".en_session_speakers a").map(&:inner_text).join(", ").presence
@@ -103,15 +98,13 @@ class Talk < ActiveRecord::Base
     end
     
     def create_railsconf_2007_talks
-      sessions_html = RestClient.get("http://conferences.oreillynet.com/pub/w/51/sessions.html")
-      sessions_doc = Nokogiri::HTML(sessions_html)
+      sessions_doc = fetch_and_parse("http://conferences.oreillynet.com/pub/w/51/sessions.html")
       
       sessions_doc.css(".s .summary a.url").each do |elm|
         link_text = elm.inner_text.to_s.downcase.squish
         next if link_text == "welcome" || link_text == "keynote"
         
-        talk_url = "http://conferences.oreillynet.com#{elm["href"]}"
-        talk_doc = Nokogiri::HTML(RestClient.get(talk_url))
+        talk_doc = fetch_and_parse("http://conferences.oreillynet.com#{elm["href"]}")
         
         title = talk_doc.css("#session_view h2").inner_text
         speaker = talk_doc.css("#session_view > p > a").map(&:inner_text).join(", ").presence
@@ -120,7 +113,7 @@ class Talk < ActiveRecord::Base
         next if abstract.blank?
         
         bio = talk_doc.css("#session_view > p > a").map do |bio_link|
-          bio_doc = Nokogiri::HTML(RestClient.get("http://conferences.oreillynet.com#{bio_link["href"]}"))
+          bio_doc = fetch_and_parse("http://conferences.oreillynet.com#{bio_link["href"]}")
           bio_doc.css(".bio").inner_text.presence
         end.compact.join("\n")
         
