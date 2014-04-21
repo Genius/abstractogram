4f3fcbc5b8f9bce84072d44fcec0471d11a99dd2
Todd Schneider <todd.w.schneider@gmail.com>
2014-04-20 20:07:30 -0400

    no basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with :name => ENV["BASICAUTH_NAME"].to_s, :password => ENV["BASICAUTH_PASSWORD"].to_s
 end
